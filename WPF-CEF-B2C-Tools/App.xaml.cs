a98837248f4a804fe6ccff0851ddf579562f916a
Xing <jiangxing@refordom.com>
2021-12-20 19:14:41 +0800

    第一天：产品技术选项 WPF .net Core Cefsharp 第二天：计划优先级 -- taobao 订单采集

---
+++ WPF-CEF-B2C-Tools/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_CEF_B2C_Tools
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ WPF-CEF-B2C-Tools/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Windows;
+
+namespace WPF_CEF_B2C_Tools {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e) {
+            CefSettings settings = new CefSettings();
+            settings.CachePath = System.AppDomain.CurrentDomain.BaseDirectory + "/cef_cache";
+            
+            if (!Cef.IsInitialized)
+            {
+                Cef.Initialize(settings);
+            }
+
+        }
+
+
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e) {
+
+        }
+
+
+        private void menuTaobaoOrder_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
